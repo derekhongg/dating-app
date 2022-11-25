343d5dcdb8c1695f9349d4ece022f48af7b9572c
derekhongg <hongderek@gmail.com>
2022-11-25 10:08:56 -0800

    setting up sign up page

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import {React, useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie'
 
 function SignUp() {
+    const [cookies, setCookies, removeCookies] = useCookies(null);
+    const [form, setForm] = useState({
+        user_id: cookies.UserId,
+        first_name: "",
+        dob_day: "",
+        dob_month: "",
+        dob_year: "",
+        show_gender: false,
+        gender_identity: "man",
+        gender_interest: "woman",
+        url: "",
+        about: "",
+        matches: []
+    })
+
+    const navigate = useNavigate();
     return (
         <div>Home</div>
     )
