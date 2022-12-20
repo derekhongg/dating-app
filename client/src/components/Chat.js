0ebfc4af0a92f122a1cf53374b466d56baa5e63b
derekhongg <hongderek@gmail.com>
2022-12-20 15:45:48 -0800

    added chat input

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function Chat() {
-  return (
-    <div>Chat</div>
-  )
+    return (
+        <div>Chat</div>
+    )
 }
 
 export default Chat
--- client/src/components/ChatInput.js
+++ client/src/components/ChatInput.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function ChatInput() {
-  return (
-    <div>ChatInput</div>
-  )
+function ChatInput({ user, clickedUser, getUserMessages, getCLickedUserMessages }) {
+
+    const [textArea, setTextArea] = useState("");
+    const userId = user?.user_id;
+    const clickedUserId = clickedUser?.user_id;
+
+    const addMessage = async () => {
+        const message = {
+            timestamp: new Date().toISOString(),
+            from_userId: userId,
+            to_userId: clickedUserId,
+            message: textArea
+        }
+        try {
+            await axios.post('http://localhost:8000/message', { message })
+            getUserMessages();
+            getCLickedUserMessages();
+            setTextArea("")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <div className="chat-input">
+            <textarea value={textArea} onChange={(e) => setTextArea(e.target.value)} />
+            <button className="secondary-button" onClick={addMessage}>Submit</button>
+        </div>
+    )
 }
 
 export default ChatInput
